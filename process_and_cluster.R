66d7c1c9fcdb4751d96ba2230c748675513a07e8
andrewbernier <andrew.g.bernier@gmail.com>
2020-08-25 01:02:14 -0500

    got up to running pca

--- process_and_cluster.R
+++ process_and_cluster.R
@@ -1,19 +1,49 @@
+library(recipes)
+library(psych)
+set.seed(123)
+
 # Data Processing
-df_pca <- df %>% select(-name,-artists, -id,
-                        -release_date, -explicit,
-                        -key, -mode, -year)
+df_clean <- df %>%
+  # Filtering out some songs that we don't want to include
+  # Most of these are white noise tracks
+  filter(!artists %in% c('[\'Sound Dreamer\']', '[\'Sounds for Life\']',
+                         '[\'Lightning, Thunder and Rain Storm\']',
+                         '[\'Ocean Sounds\']', '[\'Ocean Waves For Sleep\']',
+                         '[\'Sleep\']', '[\'Rain Sounds\']',
+                         '[\'Unspecified\']')) %>% 
+  filter(!(speechiness == 0 & tempo == 0 & valence == 0)) %>% 
+  # Probably controversial but I'm removing tracks nobody listens to
+  filter(!popularity == 0) %>% 
+  # Removing some of the variables we won't use for PCA
+  select(-name,-artists,-id, -release_date, -explicit, -key, -mode, -year)
+
+# Scale and center the data
+df_pca <- recipe(popularity ~ ., data=df_clean) %>% 
+  step_normalize(all_numeric()) %>% 
+  prep() %>% 
+  bake(new_data = df_clean) %>% 
+  # Going to remove the duration variable because I think
+  # the outliers might make the PCA really weird due to outliers
+  select(-duration_ms,
+         -popularity)
+
+# Look at new distribution of variables
+plot_num(df_pca)
+
+
+
+
+#########################################
+## this is where I stopped ##
+#########################################
+
 
 
 
 
 #Clustering
-data_kmeans <- df %>% select(-name,-artists,-id, -release_date, -explicit, -key, -mode, -year)
-
-#Scale
-data_scale <- scale(data_kmeans)
-
 #Identify Optimal Clusters
-library(psych)
+
 fa.parallel(correlation, n.obs=169909, fa="both", n.iter=100, show.legend=TRUE,main="Scree Plot (Parallel Analysis)")
 fa(correlation, n.obs=169909, n.iter=100, show.legend=TRUE)
 
