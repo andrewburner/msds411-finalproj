fd6d8af18c4383cb606bc6a97987ea29d1116ba7
andrewbernier <andrew.g.bernier@gmail.com>
2020-08-26 09:30:13 -0500

    update through clustering

--- process_and_cluster.R
+++ process_and_cluster.R
@@ -13,9 +13,7 @@ df_clean <- df %>%
                          '[\'Unspecified\']')) %>% 
   filter(!(speechiness == 0 & tempo == 0 & valence == 0)) %>% 
   # Probably controversial but I'm removing tracks nobody listens to
-  filter(!popularity == 0) %>% 
-  # Removing some of the variables we won't use for PCA
-  select(-name,-artists,-id, -release_date, -explicit, -key, -mode, -year)
+  filter(!popularity == 0)
 
 # Scale and center the data
 df_pca <- recipe(popularity ~ ., data=df_clean) %>% 
@@ -23,36 +21,51 @@ df_pca <- recipe(popularity ~ ., data=df_clean) %>%
   prep() %>% 
   bake(new_data = df_clean) %>% 
   # Going to remove the duration variable because I think
-  # the outliers might make the PCA really weird due to outliers
-  select(-duration_ms,
-         -popularity)
+  # it might make the PCA really weird due to outliers
+  select(-duration_ms, -year, -mode,
+         -popularity, -mode, -explicit, -key,
+         -name, -artists, -release_date, -id)
 
 # Look at new distribution of variables
 plot_num(df_pca)
 
-
-
-
-#########################################
-## this is where I stopped ##
-#########################################
-
-pca_data <- prcomp(df_pca)
+# Get PCs
+pca_data <- princomp(df_pca)
+summary(pca_data)
 fviz_eig(pca_data)
 
+# Add PCs to the data set
+df_clean <- df_clean %>% mutate(PC1 = pca_data$scores[,1],
+                            PC2 = pca_data$scores[,2],
+                            PC3 = pca_data$scores[,3],
+                            PC4 = pca_data$scores[,4],
+                            PC5 = pca_data$scores[,5],
+                            PC6 = pca_data$scores[,6])
 
 #Clustering
 #Identify Optimal Clusters
+correlation <- df_clean %>% select_if(is.numeric) %>% cor()
 
-fa.parallel(correlation, n.obs=169909, fa="both", n.iter=100, show.legend=TRUE,main="Scree Plot (Parallel Analysis)")
-fa(correlation, n.obs=169909, n.iter=100, show.legend=TRUE)
+fa.parallel(correlation, fa="both", n.iter=100, show.legend=TRUE,main="Scree Plot (Parallel Analysis)")
+fa(correlation, n.obs = 142397, n.iter=100, show.legend=TRUE, fm = 'ml')
 
 #Kmeans
-kmeans_data <- kmeans(data_scale, centers = 6, nstart = 25)
-kmeans_data
+kmeans_data_pca <- df_clean %>% select(PC1, PC2, PC3, PC4, PC5, PC6) %>% 
+  kmeans(centers = 7, nstart = 25)
+
+kmeans_data <- df_pca %>% 
+  kmeans(centers = 7, nstart = 25)
 
 #Cluster Plot
-fviz_cluster(kmeans_data, data = data_scale, geom="point")
+fviz_cluster(kmeans_data_pca, data = df_clean, geom="point")
+fviz_cluster(kmeans_data, data = df_pca, geom="point")
+
+
+
+################################
+### This is where I left off ###
+################################
+
 
 #Distributions in Clusters
 centers <- data.frame(cluster = rownames(kmeans_data$centers), kmeans_data$centers)
